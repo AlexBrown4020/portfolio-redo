191a3737390d302f30e3716f77053b8e828f8e22
AlexBrown4020 <balex3497@gmail.com>
2022-11-16 10:56:04 +0900

    Feat: Implement scrolling appearance for cards

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var cl = new cloudinary.Cloudinary({cloud_name: "", secure: true});
+// var cl = new cloudinary.Cloudinary({cloud_name: "", secure: true});
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
